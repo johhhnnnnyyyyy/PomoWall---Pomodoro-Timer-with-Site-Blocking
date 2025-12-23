4d9e13d2667c7e0022c14396a0f57746d60cc6b3
Johnston P S <2007.johnston@gmail.com>
2025-12-23 16:23:42 +0530

    less gooo figured out to switch slide!!baaam

--- xtension.js
+++ xtension.js
@@ -1,19 +1,27 @@
 //code for switching slides
 
 const slides=document.querySelectorAll('.slide');
-const arrows=document.querySelectorAll('.material-symbols-outlined.A');
+const arrows=document.querySelectorAll('.A');
 let currSlide=0;
 const totalSlide=3;  //focus=0,short break=1,long break=2
 
 function prevSlide(){
+  slides[currSlide].classList.remove('active');
   currSlide=(currSlide-1+totalSlide)%3;
-  showSlide();
+  slides[currSlide].classList.add('active');
 }
 function nextSlide(){
+  slides[currSlide].classList.remove('active');
   currSlide=(currSlide+1)%3;
-  showSlide();
+  slides[currSlide].classList.add('active');
 }
 
-function showSlide(){}
+arrows.forEach(arrow => {
+  arrow.addEventListener('click',()=>{
+    if(arrow.classList.contains('left'))        prevSlide();
+    else if(arrow.classList.contains('right'))  nextSlide();
+  })
+});
 
-//PENDING - hovering effect, clickabilty, MAIN CHROME EXTENSION LOGIC
+
+//PENDING - clickabilty, MAIN CHROME EXTENSION LOGIC
